94da4f6f0f79a68059adde5336c8f4ba57221afa
Sofija Šebek <sofijasebek@gmail.com>
2020-03-28 18:09:43 +0100

    Created a blank Spring Boot project

---
+++ aukcija/src/main/java/com/safari/aukcija/AukcijaApplication.java
@@ -0,0 +1,13 @@
+package com.safari.aukcija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AukcijaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AukcijaApplication.class, args);
+	}
+
+}
