0b0c94dfb4bf2bde4736e00df814d2cf9572f8b2
Sofija Šebek <sofijasebek@gmail.com>
2020-03-29 04:12:14 +0200

    JPA project created

---
+++ SafariJPA/src/model/Predmet.java
@@ -0,0 +1,171 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+
+/**
+ * The persistent class for the predmet database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Predmet.findAll", query="SELECT p FROM Predmet p")
+public class Predmet implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idPredmet;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date krajAukcije;
+
+	private String nazivPredmeta;
+
+	private String opis;
+
+	private int pocetnaCena;
+
+	private String stanje;
+
+	private byte status;
+
+	//bi-directional many-to-one association to Licitacija
+	@OneToMany(mappedBy="predmet")
+	private List<Licitacija> licitacijas;
+
+	//bi-directional many-to-one association to Kategorija
+	@ManyToOne
+	private Kategorija kategorija;
+
+	//bi-directional many-to-one association to Korisnik
+	@ManyToOne
+	@JoinColumn(name="idProdavca")
+	private Korisnik korisnik;
+
+	//bi-directional many-to-one association to Slika
+	@OneToMany(mappedBy="predmet")
+	private List<Slika> slikas;
+
+	public Predmet() {
+	}
+
+	public int getIdPredmet() {
+		return this.idPredmet;
+	}
+
+	public void setIdPredmet(int idPredmet) {
+		this.idPredmet = idPredmet;
+	}
+
+	public Date getKrajAukcije() {
+		return this.krajAukcije;
+	}
+
+	public void setKrajAukcije(Date krajAukcije) {
+		this.krajAukcije = krajAukcije;
+	}
+
+	public String getNazivPredmeta() {
+		return this.nazivPredmeta;
+	}
+
+	public void setNazivPredmeta(String nazivPredmeta) {
+		this.nazivPredmeta = nazivPredmeta;
+	}
+
+	public String getOpis() {
+		return this.opis;
+	}
+
+	public void setOpis(String opis) {
+		this.opis = opis;
+	}
+
+	public int getPocetnaCena() {
+		return this.pocetnaCena;
+	}
+
+	public void setPocetnaCena(int pocetnaCena) {
+		this.pocetnaCena = pocetnaCena;
+	}
+
+	public String getStanje() {
+		return this.stanje;
+	}
+
+	public void setStanje(String stanje) {
+		this.stanje = stanje;
+	}
+
+	public byte getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(byte status) {
+		this.status = status;
+	}
+
+	public List<Licitacija> getLicitacijas() {
+		return this.licitacijas;
+	}
+
+	public void setLicitacijas(List<Licitacija> licitacijas) {
+		this.licitacijas = licitacijas;
+	}
+
+	public Licitacija addLicitacija(Licitacija licitacija) {
+		getLicitacijas().add(licitacija);
+		licitacija.setPredmet(this);
+
+		return licitacija;
+	}
+
+	public Licitacija removeLicitacija(Licitacija licitacija) {
+		getLicitacijas().remove(licitacija);
+		licitacija.setPredmet(null);
+
+		return licitacija;
+	}
+
+	public Kategorija getKategorija() {
+		return this.kategorija;
+	}
+
+	public void setKategorija(Kategorija kategorija) {
+		this.kategorija = kategorija;
+	}
+
+	public Korisnik getKorisnik() {
+		return this.korisnik;
+	}
+
+	public void setKorisnik(Korisnik korisnik) {
+		this.korisnik = korisnik;
+	}
+
+	public List<Slika> getSlikas() {
+		return this.slikas;
+	}
+
+	public void setSlikas(List<Slika> slikas) {
+		this.slikas = slikas;
+	}
+
+	public Slika addSlika(Slika slika) {
+		getSlikas().add(slika);
+		slika.setPredmet(this);
+
+		return slika;
+	}
+
+	public Slika removeSlika(Slika slika) {
+		getSlikas().remove(slika);
+		slika.setPredmet(null);
+
+		return slika;
+	}
+
+}
---
+++ SafariJPA/src/model/Slika.java
@@ -0,0 +1,63 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the slika database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Slika.findAll", query="SELECT s FROM Slika s")
+public class Slika implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idSlika;
+
+	private String ime;
+
+	private String putanja;
+
+	//bi-directional many-to-one association to Predmet
+	@ManyToOne
+	private Predmet predmet;
+
+	public Slika() {
+	}
+
+	public int getIdSlika() {
+		return this.idSlika;
+	}
+
+	public void setIdSlika(int idSlika) {
+		this.idSlika = idSlika;
+	}
+
+	public String getIme() {
+		return this.ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getPutanja() {
+		return this.putanja;
+	}
+
+	public void setPutanja(String putanja) {
+		this.putanja = putanja;
+	}
+
+	public Predmet getPredmet() {
+		return this.predmet;
+	}
+
+	public void setPredmet(Predmet predmet) {
+		this.predmet = predmet;
+	}
+
+}
