599353e5a79f170433a98c153ddc853e025f93df
mtesic97 <61319309+mtesic97@users.noreply.github.com>
2020-04-03 18:48:10 +0200

    method for login and register

--- aukcija/src/main/java/com/safari/aukcija/controllers/KorisnikController.java
+++ aukcija/src/main/java/com/safari/aukcija/controllers/KorisnikController.java
@@ -6,10 +6,13 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.safari.aukcija.repository.KorisnikRepository;
 import com.safari.aukcija.service.KorisnikService;
 
 import model.Korisnik;
@@ -19,13 +22,47 @@ import model.Korisnik;
 public class KorisnikController {
 
 	@Autowired
-
 	KorisnikService korisnikService;
+	
+	@Autowired
+	KorisnikRepository korisnikRepository;
+	
+	Korisnik ulogovan;
+	
+	@RequestMapping(value="/login", method={RequestMethod.POST,RequestMethod.GET}) 
+	public String login(String e_mail,String password,Model m,HttpServletRequest request){
+		ulogovan = korisnikRepository.login(e_mail, password);
+		if(ulogovan != null){
+			request.getSession().setAttribute("ulogovan", ulogovan);
+		}else{
+			m.addAttribute("porukaLogin", "Uneli ste pogresne podatke");
+		}
+		
+		return "Logovanje";
+	}
+	
+	@PostMapping(value="registracija")
+	public String registracija(Korisnik k ,HttpServletRequest request){
+		try{
+			k=korisnikService.addKorisnik(k);
+			if(k != null){
+				request.getSession().setAttribute("porukaR", "Uspesno ste registrovani.");
+			}else{
+				request.getSession().setAttribute("porukaR", "Greska prilikom registracije"); 
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return "Registracija";
+	}
 	
 	@RequestMapping("/getAllKorinsik")
 	public List<Korisnik> getAllKorisnik(){
 		return korisnikService.getAll();
 	}
+	
+	
 	
 	@RequestMapping("/saveKorisnik")
 	public Korisnik saveKorisnik(HttpServletRequest request) {
