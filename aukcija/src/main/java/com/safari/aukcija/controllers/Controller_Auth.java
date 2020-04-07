38aedd21169020ead2f0aabe2d9d1af94d2a8887
Sofija Šebek <sofijasebek@gmail.com>
2020-04-07 16:54:17 +0200

    unused import

--- aukcija/src/main/java/com/safari/aukcija/controllers/Controller_Auth.java
+++ aukcija/src/main/java/com/safari/aukcija/controllers/Controller_Auth.java
@@ -2,8 +2,6 @@ package com.safari.aukcija.controllers;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.web.bind.annotation.RequestBody;
